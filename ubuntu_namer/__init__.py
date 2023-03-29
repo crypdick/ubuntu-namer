bf54c28f1fbdc065392baef0002075d1042b2edf
Richard Decal <richard.decal@ncf.edu>
2023-03-28 22:36:54 -0400

    mv main.py to __main__.py

--- ubuntu_namer/__init__.py
+++ ubuntu_namer/__init__.py
@@ -1,7 +1,7 @@
 # type: ignore[attr-defined]
 """Awesome `ubuntu-namer` is a Python cli/package created with https://github.com/TezRomacH/python-package-template"""
 
-from .main import generate_name
+from .__main__ import generate_name
 from .names import ubuntu_names
 
 # from importlib import metadata as importlib_metadata
